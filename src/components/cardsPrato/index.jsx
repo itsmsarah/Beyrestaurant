6ba08446a665310aaab196b608e41839793ad9ae
itsmsarah <sarahmendesandrade1@gmail.com>
2025-12-22 21:55:01 -0300

    feat:Finish the pratos em destaque section and the cards components

--- src/components/cardsPrato/index.jsx
+++ src/components/cardsPrato/index.jsx
@@ -2,11 +2,18 @@ import './style.css'
 export const CardPrato = ({imgCardPrato,h3,p}) =>{
     return(
         <>
-        <div className="card-prato-container">
+      
+        <div className="card-prato-contet">
             <img src={imgCardPrato} alt="" />
+            <div className="card-prato-descricao">
+
                 <h3>{h3}</h3>
                 <p>{p}</p>
             </div>
+            </div>
+      
+    
+           
        
         </>
     )
--- src/components/layouts/sectionPratos/index.jsx
+++ src/components/layouts/sectionPratos/index.jsx
@@ -1,18 +1,43 @@
 import './style.css'
 import { CardPrato } from "../../cardsPrato"
 import imgPrato1 from '../../../assets/images/prato-destaque-1.svg'
-
+import imgPrato2 from '../../../assets/images/prato-destaque-2.svg'
+import imgPrato3 from '../../../assets/images/prato-destaque-3.svg'
+import imgPrato4 from '../../../assets/images/prato-destaque-4.svg'
 
 export const PratosDestaque = () =>{
     return(
         <>
         <section className="pratos-destaque">
+            <h2>Pratos em <span>Destaque</span></h2>
+            <div className="pratos-destaque-container">
 
             <div className="cards-pratos-container">
                 <CardPrato imgCardPrato={imgPrato1}
                 h3="Bacon & Ovos Gourmet"
-                p="Ovos fritos no ponto perfeito, acompanhados de fatias crocantes de bacon, batatas douradas, tomatinhos frescos, rabanete e folhas verdes selecionadas."/>
+                p="Ovos fritos no ponto perfeito, acompanhados de fatias crocantes de bacon, batatas douradas, 
+                tomatinhos frescos, rabanete e folhas verdes selecionadas."/>
             </div>
+            <div className="cards-pratos-container">
+                <CardPrato imgCardPrato={imgPrato2}
+                h3="Bife Grelhado  Gourmet"
+                p="Suculento bife grelhado ao ponto, servido com rúcula fresca, tomates assados,
+                 e uma rodela de limão siciliano tostado para realçar os sabores."/>
+            </div>
+            <div className="cards-pratos-container">
+                <CardPrato imgCardPrato={imgPrato3}
+                h3="Fusilli Mediterrâneo"
+                p="Massa fusilli al dente, envolta em molho de tomate fresco, berinjela assada, 
+                tomates cereja e manjericão fresco, finalizada com um toque de azeite extra virgem."/>
+            </div>
+            <div className="cards-pratos-container">
+                <CardPrato imgCardPrato={imgPrato4}
+                h3="Spaghetti Gourmet"
+                p="Massa al dente envolta em rico molho bolognesa artesanal, com tomates cereja selecionados
+                , parmesão ralado e folhas frescas de manjericão."/>
+            </div>
+            </div>
+
 
                <a className='btn-primary' href="">Ver cardapio</a>
         </section>
